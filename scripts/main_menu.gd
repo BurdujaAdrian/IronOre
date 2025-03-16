f399ec39a167d867bdd45c55ff6d65c6f4a6ef87
BurdujaAdrian <adrianburduja7.3@gmail.com>
2025-03-16 18:13:57 +0200

    added animation to text

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -1,16 +1,16 @@
 extends Node2D
 
-var dialogue_scene = preload("res://generic_dialogue.tscn").instantiate()
+var dialogue_scene = preload("res://scenes/generic_dialogue.tscn").instantiate()
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	$ui/new.pressed.connect(_new_game)
-	
+	$ui/exit.pressed.connect(_exit_game)
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 
 	pass
 
@@ -20,4 +20,8 @@ func _new_game():
 
 	get_tree().root.add_child(dialogue_scene)
 	get_tree().root.remove_child($"..")
+	pass
+
+func _exit_game():
+	get_tree().quit(0)
 	pass
