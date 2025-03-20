c7d0a7c4477526e957a70f7348f2f2116662cc51
BurdujaAdrian <adrianburduja7.3@gmail.com>
2025-03-20 22:46:55 +0200

    added minimal save system

--- IronOre/scripts/global.gd
+++ IronOre/scripts/global.gd
@@ -4,14 +4,14 @@ var global_true = true
 
 var study:int
 
-var lang_progress:int
+var lang:int
 
 var stress:int
 
-var week:int
 
-var route:routes
-enum routes{generic,gpt}
+var week:int     = 0
+var route:routes = routes.prologue; enum routes{main,gpt,prologue}
+var scene:int    = 1
 
 var last_choice:game_choice
 var curr_choice:game_choice
@@ -23,13 +23,59 @@ func _ready() :
 		
 	pass
 
+
+var save_path =  "user://player_data.ini"
+
 func save_game() :
 	print("saving game...")
+	
+	var config_file = ConfigFile.new()
+	
+	config_file.set_value("Variables", "lang", lang)
+	config_file.set_value("Variables", "stress", stress)
+	config_file.set_value("Variables", "study", study)
+	
+	config_file.set_value("Progress", "week",week)
+	config_file.set_value("Progress","route",route)
+	config_file.set_value("Progress","scene",scene)
+	config_file.set_value("Progress", "last_choice",last_choice)
+	config_file.set_value("Progress","curr_choice",curr_choice)
+	
+	var error = config_file.save(save_path)
+	if error:
+		print("Error saving game progress: ",error)
 	pass
 	
-func load_game():
+func load_game() -> Node:
 	print("loading game ...")
-	pass
+	
+	var config_file = ConfigFile.new()
+	var error = config_file.load(save_path)
+
+	if error != ERR_FILE_NOT_FOUND && error:		
+		print("Error loading save file: ",error)
+		return null
+	
+	lang = config_file.get_value("Variables", "lang", lang)
+	stress = config_file.get_value("Variables", "stress", stress)
+	study = config_file.get_value("Variables", "study", study)
+	
+	week = config_file.get_value("Progress", "week",week)
+	route = config_file.get_value("Progress","route",route)
+	scene = config_file.get_value("Progress","scene",scene)
+	last_choice = config_file.get_value("Progress", "last_choice",last_choice)
+	curr_choice =  config_file.get_value("Progress","curr_choice",curr_choice)
+	
+	var scene_path
+	
+	if week == 0:
+		scene_path ="res://scenes/prologue/scene%s.tscn" % [scene]
+	else:
+		scene_path = "res://scenes/week%s/scene_%s%s.tscn" % [week,route,scene]
+	
+	var scene_node = load(scene_path).instantiate()
+	
+	return scene_node
 	
 
 func reset_game():
--- IronOre/scripts/main_menu.gd
+++ IronOre/scripts/main_menu.gd
@@ -2,14 +2,16 @@ extends Node2D
 
 var dialogue_scene = preload("res://scenes/generic_dialogue.tscn").instantiate()
 
+var curr_scene
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	if Global.global_true:
 		print("true")
 	$ui/new.pressed.connect(_new_game)
+	$ui/load.pressed.connect(_load_game)
 	$ui/exit.pressed.connect(Global.exit)
 	
-	Global.load_game()
+	curr_scene = Global.load_game()
 	pass # Replace with function body.
 
 
@@ -19,9 +21,18 @@ func _process(_delta: float) -> void:
 	pass
 
 
+func _load_game():
+	print("open previous game")
+
+	curr_scene = Global.load_game()
+	get_tree().root.add_child(curr_scene)
+	get_tree().root.remove_child($"..")
+	pass
+
 func _new_game():
 	print("make new game")
 
-	get_tree().root.add_child(dialogue_scene)
+	curr_scene = Global.reset_game()
+	get_tree().root.add_child(curr_scene)
 	get_tree().root.remove_child($"..")
 	pass
