71cb6f657579722863301d0b7528920ef86f52f0
BurdujaAdrian <adrianburduja7.3@gmail.com>
2025-03-20 18:54:18 +0200

    Added gameplay scene and functions for the corresponding buttons. TODO: add actual functionality to the buttons

--- scripts/generic_dialogue.gd
+++ scripts/generic_dialogue.gd
@@ -1,25 +1,33 @@
 extends Node2D
 
-@onready var dialogue_box = $ui/PanelContainer/RichTextLabel
-@onready var text_length:int = 0
-@onready var display_text_len:float = 0
 var text_speed = 60
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	$ui/exit.pressed.connect(_exit_game)
-	text_length = len(dialogue_box.text)
+	
+	$ui/learn.pressed.connect(_learn)
+	$ui/relax.pressed.connect(_relax)
+	$ui/work.pressed.connect(_relax)
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	
-	if dialogue_box.visible_characters < text_length:
-		display_text_len += delta*text_speed
-		dialogue_box.visible_characters = floor(display_text_len)
-		print("Increase text length, text is ",dialogue_box.visible_characters )
 	pass
 
 func _exit_game():
 	get_tree().quit(0)
+	pass
+
+func _learn() -> void:
+	print("time to learn")
+	pass
+
+func _work() -> void:
+	print("time to work")
+	pass
+	
+func _relax()->void:
+	print("time to chill")
 	pass
